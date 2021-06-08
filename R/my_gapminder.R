221c8d29d6b27ef3ccf0b54c5d0d8880b5cdcc52
unknown <huangyuxin2018@sina.com>
2021-06-08 15:33:15 +0800

    add gapminder data change

--- R/my_gapminder.R
+++ R/my_gapminder.R
@@ -23,6 +23,7 @@
 #' @seealso \code{\link[gapminder]{country_colors}} for a nice color scheme for the countries
 #' @importFrom tibble tibble
 #' @examples
+#' gapminder <- my_gapminder
 #' str(gapminder)
 #' head(gapminder)
 #' summary(gapminder)
@@ -30,7 +31,7 @@
 #' aggregate(lifeExp ~ continent, gapminder, median)
 #' plot(lifeExp ~ year, gapminder, subset = country == "Cambodia", type = "b")
 #' plot(lifeExp ~ gdpPercap, gapminder, subset = year == 2007, log = "x")
-#' gapminder <- my_gapminder
+#'
 #'
 #' if (require("dplyr")) {
 #' gapminder %>%
