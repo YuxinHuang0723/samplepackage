72900e39dd771479122e6bacaa127b991293cf0d
unknown <huangyuxin2018@sina.com>
2021-06-08 16:28:52 +0800

    add change to my_t.test test function

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,8 +1,8 @@
 # within test-my_t.test.R
 test_that("my_t.test work for testing a hypothesis", {
-  expect_equal(my_t.test(gapminder$lifeExp, "greater", 60),
+  expect_equal(my_t.test(my_gapminder$lifeExp, "greater", 60),
                -1.68, 1703, 0.9534296, "greater")
 })
 test_that("non-suitable input throws informative error", {
-  expect_error(my_t.test(gapminder$lifeExp, "greater than", 60))
+  expect_error(my_t.test(my_gapminder$lifeExp, "greater than", 60))
 })
